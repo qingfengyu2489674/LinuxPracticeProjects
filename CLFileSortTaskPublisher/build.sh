02eae99892d03252fae0db269d9a3305879f9f65
DavidingPlus <davidingplus@qq.com>
2024-12-13 14:59:04 +0800

    mv all source code to dir src

--- CLFileSortTaskPublisher/build.sh
+++
@@ -1,1 +0,0 @@
-g++ -std=c++17 -pthread ./CLFileDataSorter/CLDataSorter.cpp ./CLFileReadManager/CLFileReadManager.cpp ./CLFileSortTaskPublisher.cpp ./CLSortedFileMerger/CLSortedFileMerger.cpp ./main.cpp -o main
--- build.sh
+++
@@ -1,9 +0,0 @@
-g++ -std=c++17 -pthread \
-    ./CLFileSortTaskPublisher/CLFileDataSorter/CLDataSorter.cpp \
-    ./CLFileSortTaskPublisher/CLFileReadManager/CLFileReadManager.cpp \
-    ./CLFileSortTaskPublisher/CLFileSortTaskPublisher.cpp \
-    ./CLFileSortTaskPublisher/CLSortedFileMerger/CLSortedFileMerger.cpp \
-    ./CLThreadPool/CLThreadPool.cpp \
-    ./CLCooridinator.cpp \
-    ./main.cpp \
-    -g -o main
